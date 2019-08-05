9bf08d4086713502a3d2383a22e0cfb7fc5a48a3
Sach97 <sacha.arbonel@hotmail.fr>
2019-08-05 23:16:59 +0200

    more tests to pair like ranks

--- src/hand/mod.rs
+++ src/hand/mod.rs
@@ -1,8 +1,6 @@
 #![feature(slice_partition_dedup)]
 pub mod rank;
 use rank::Rank;
-// mod utils;
-// use utils;
 //use std::fmt::{Display, Formatter, Result};
 //use std::array::FixedSizeArray;
 
@@ -22,12 +20,11 @@ impl Default for Hand {
 
 }
 
-  fn handle_three_or_pair_kind(mut suits : Vec<Suit>) -> Rank { 
-        let (_, _dup_hand) = suits.partition_dedup();
+  fn handle_three_or_pair_kind(mut faces : Vec<Face>) -> Rank { //hacky way, put this in the trait and fight the compiler
+        let (_, _dup_hand) = faces.partition_dedup();
         match _dup_hand.len() {   
-         0 =>Rank::TwoPairs,
-         2 => Rank::ThreeOfAKind,
-        _ => Rank::None,
+         0 => Rank::TwoPairs,
+         _ => Rank::ThreeOfAKind,
         }
     }
 
@@ -58,20 +55,16 @@ impl Hand {
     fn suits(&self) -> Vec<Suit> {
         self.0.iter().map(|card| card.suit).collect()
     }
-
   
-    
     pub fn rank(&mut self) -> Rank {
-        self.0.sort();
-        let mut suits = self.suits();
-        let (dedup, _dup_hand) = suits.partition_dedup();
-
+        let mut faces = self.faces();
+        faces.sort();
+        let (_, _dup_hand) = faces.partition_dedup();
         match _dup_hand.len() {
-            
          1 => Rank::Pair,
          2 => handle_three_or_pair_kind(_dup_hand.to_vec()),
          3 => Rank::FourOfAKind,
-        _ => Rank::None,
+         _ => Rank::None,
         }
     }
 
@@ -119,4 +112,35 @@ mod tests {
         assert_eq!(hand, Hand::from_vec(vec!["2h", "3d", "5s", "9c", "Kd"]));
     }
 
+    #[test]
+    fn test_rank_pair() {
+         let mut hand = Hand::from_vec(vec!["2d", "2h", "3d", "5s", "9c"]);
+         let rank = hand.rank();
+         println!("{:?}",rank);
+         assert_eq!(rank, Rank::Pair);
+    }
+
+    #[test]
+    fn test_rank_two_pairs() {
+         let mut hand = Hand::from_vec(vec!["2d", "2h", "3s", "3s", "9c"]);
+         let rank = hand.rank();
+         println!("{:?}",rank);
+         assert_eq!(rank, Rank::TwoPairs);
+    }
+
+     #[test]
+    fn test_rank_three_kind() {
+         let mut hand = Hand::from_vec(vec!["2d", "2c", "2s", "3s", "9c"]);
+         let rank = hand.rank();
+         println!("{:?}",rank);
+         assert_eq!(rank, Rank::ThreeOfAKind);
+    }
+    
+     #[test]
+    fn test_rank_four_kind() {
+         let mut hand = Hand::from_vec(vec!["2d", "2c", "2s", "2h", "9c"]);
+         let rank = hand.rank();
+         println!("{:?}",rank);
+         assert_eq!(rank, Rank::FourOfAKind);
+    }
 }
